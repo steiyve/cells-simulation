f191a33b75f236132ca468ce4a9f7fdda7a57507
steiyve <94018684+steiyve@users.noreply.github.com>
2023-10-11 15:28:49 -0400

    Update cellule.py

--- cellule.py
+++ cellule.py
@@ -1,1 +1,35 @@
+import turtle
+import math
+import random
+
+def random_position():
+    x = random.randint(-200, 200)
+    y = random.randint(-200, 200)
+    return x, y
+
+def duplicat(num):
+	num_turtles = num
+	return num * 2
+
+num_turtles = duplicat(2)
+turtles = []
+gen = 0
+
+def creat_new_turtle():
+	global turtles
+	repete = len(turtles)
+	for i in range(repete*2):
+		t = turtle.Turtle()
+		t.penup()
+		t.goto(*random_position())
+		turtles.append(t)
+
+while True:
+    for t in turtles:
+        t.goto(*random_position())
+	
+	if gen % 2 == 0:
+		create_new_turtles()
+		
+turtle.done()
 
