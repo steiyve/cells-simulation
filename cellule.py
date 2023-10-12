5c5017f5ee802e18d0e71cdcf9d3714a349fd28c
Nicolas Prigge <priggen@educ.college-montreal.qc.ca>
2023-10-12 12:54:52 -0400

    bug patch

--- cellule.py
+++ cellule.py
@@ -3,8 +3,8 @@ import math
 import random
 
 def random_position():
-    x = random.randint(-200, 200)
-    y = random.randint(-200, 200)
+    x = random.randint(-400, 400)
+    y = random.randint(-400, 400)
     return x, y
 
 def duplicat(num):
@@ -15,11 +15,29 @@ num_turtles = duplicat(2)
 turtles = []
 gen = 0
 
+for i in range(2):
+		t = turtle.Turtle()
+		t.color("green")
+		t.shape("circle")
+		t.begin_fill()
+		#mutation
+		if random.randint(0,20) == 20:
+			t.color("red")
+			t.name = f"mutated"
+			if random.randint(0,2) == 2:
+				t.name = f"mutated-infectious"
+		
+		t.penup()
+		
+		t.goto(*random_position())
+		turtles.append(t)
+
 def creat_new_turtle():
 	global turtles
 	repete = len(turtles)
 	for i in range(repete*2):
 		t = turtle.Turtle()
+		t.shape("circle")
 		t.color("green")
 		t.begin_fill()
 		#mutation
@@ -34,11 +52,10 @@ def creat_new_turtle():
 		turtles.append(t)
 
 while True:
-    for t in turtles:
-        t.goto(*random_position())
-	
-	if gen % 2 == 0:
-		create_new_turtles()
+
+	if gen % 10 == 0:
+		creat_new_turtle()
 		
+	gen += 1
 turtle.done()
 
